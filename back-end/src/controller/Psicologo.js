b010ab7e2ff3b92212db09ab1fb85c3522c06298
aluno <e023aluno@outlook.com>
2026-05-04 22:16:35 -0300

    criacao do banco e das colecoes usuarios e psicologos

---
+++ back-end/src/controller/Psicologo.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const psicologoSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+     email:{
+        type: String,
+        required: true,
+    },
+     crp:{
+        type: String,
+        required: true,
+    },
+     especialidade:{
+        type: [String],
+        required: true,
+        default: []
+    },
+     preco_sessao:{
+        type: Number,
+        default: 0,
+        required: true,
+    },
+     bio:{
+        type: String,
+        Default: 'patient',
+        required: true,
+    },
+     Status:{
+        type: String,
+        Enum: ['pendente', 'suspenso'],
+        Default: 'pendente',
+        required: true,
+    }   
+},
+    {
+        timestamps: true
+    },
+    {
+        versionKey: false
+    }
+);
+
+const Psicologos = mongoose.model('Psicologos', psicologoSchema);
---
+++ back-end/src/model/ModelUsuario.js
@@ -0,0 +1,45 @@
+import mongoose, { version } from "mongoose";
+
+const usuarioSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+     email:{
+        type: String,
+        required: true,
+    },
+     senha:{
+        type: String,
+        required: true,
+    },
+     dataNascimento:{
+        type: String,
+        required: true,
+    },
+     pontosFidelidade:{
+        type: String,
+        default: 0,
+        required: true,
+    },
+     role:{
+        type: String,
+        Default: 'patient',
+        required: true,
+    },
+     Status:{
+        type: String,
+        Enum: ['ativo', 'suspenso'],
+        Default: 'ativo',
+        required: true,
+    }   
+},
+    {
+        timestamps: true
+    },
+    {
+        versionKey: false
+    }
+);
+
+const Usuarios = mongoose.model('Usuarios', usuarioSchema);
